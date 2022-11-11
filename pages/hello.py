6f2a339372a811e5182e713babddd2cdadedff24
Ark <arkinsight7@gmail.com>
2022-11-12 04:38:08 +0900

    first commit

---
+++ pages/hello.py
@@ -0,0 +1,23 @@
+# sudo apt install python3-pip  //pip 설치 (파이썬 터미널에 입력할 것)
+# pip install gradio            //gradio 설치
+
+# 설치 성공은 했는데 이런 경고가 뜨네?
+#  WARNING: The script uvicorn is installed in '/home/ark/.local/bin' which is not on PATH.
+#   Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+
+# import gradio as gr
+
+# def greet(name) :
+#     return "Hello" + name + "!!"
+
+# iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+# iface.launch()
+
+print("Hello World")
+
+# C:\\Users\\donghwankim\\AppData\\Local\\Programs\\Python\\Python310\\python.exe
+
+# import subprocess
+# subprocess.call([r'/home/ark/aiart-app/stable-diffusion-webui/webui-user.bat'])
+
+# webui-user.bat 실행 부터 다시 하면 됨
